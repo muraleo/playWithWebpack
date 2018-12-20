146b5a5f53d97df30b5854a498e7840e28eadd79
muraleo <muraleo80923@gmail.com>
2018-12-20 14:17:00 -0500

    webpack development configs

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,28 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
+    // this webpack.config is just for development
+    mode: 'development',
     entry: {
         app:'./src/index.js',
         print: './src/print.js'
+    },
+    // srouce map can help track down errors in browser, not in terminal
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     plugins:[
+        // clean dist folder, remove all unused files
         new CleanWebpackPlugin(['dist']),
         new ManifestPlugin({
             fileName: '../manifest.json'
         }),
+        // generate a new html file, attach all bundler.js in that index.html
         new HtmlWebpackPlugin({
             title: 'Ouput Management'
         })
